74879ebf5f0ae8341d7a3bceb211b40e71c549a3
AlidaSchefers <alidadschefers@gmail.com>
2020-12-19 17:32:59 -0500

    initial commit

--- gorestapi/index.js
+++ gorestapi/index.js
@@ -38,8 +38,8 @@ function startPageElements() {
 
     const femaleGenderOption = document.createElement("option")
     const maleGenderOption = document.createElement("option")
-    const ActiveStatusOption = document.createElement("option")
-    const InactiveStatusOption = document.createElement("option")
+    const activeStatusOption = document.createElement("option")
+    const inactiveStatusOption = document.createElement("option")
 
     postUserInputBoxName.name = "name"
     postUserInputBoxEmail.name = "email"
@@ -54,15 +54,63 @@ function startPageElements() {
     maleGenderOption.text = "Male"
     postUserSelectGender.add(maleGenderOption)
 
-    ActiveStatusOption.text = "Active"
-    postUserSelectStatus.add(ActiveStatusOption)
-    InactiveStatusOption.text = "Inactive"
-    postUserSelectStatus.add(InactiveStatusOption)
+    activeStatusOption.text = "Active"
+    postUserSelectStatus.add(activeStatusOption)
+    inactiveStatusOption.text = "Inactive"
+    postUserSelectStatus.add(inactiveStatusOption)
     
     postUserButton.innerText = "Create New User"
     postUserButton.onclick = postUserRequest
 
     postDiv.id = "postDiv"
+
+    //PUT
+
+    const putDiv = document.createElement('div')
+    
+    const putUserInputBox = document.createElement("input")
+    const putUserCurrentDataButton = document.createElement("button")
+        //element properties
+    putUserInputBox.placeholder = "Enter a user's ID to search"
+
+    putUserInputBox.type = 'number'
+    putUserInputBox.min = 1
+    putUserInputBox.max = 2000
+
+    // putUserCurrentDataButton.className = "buttons" //what is the purpose of assigning a class name?
+    putUserCurrentDataButton.innerText = "Display User's Current Data"
+    putUserCurrentDataButton.onclick =  putUserDisplayCurrentData
+
+    const putUserCurrentName = document.createElement('input')
+    const putUserCurrentEmail = document.createElement('input')
+    const putUserCurrentGender = document.createElement('select')
+    const putUserCurrentStatus = document.createElement('select')
+
+    putUserCurrentName.name = "name"
+    putUserCurrentEmail.name = "email"
+    putUserCurrentGender.name = "gender"
+    putUserCurrentStatus.name = "status"
+
+    const femaleGenderOption2 = document.createElement("option")
+    const maleGenderOption2 = document.createElement("option")
+    const activeStatusOption2 = document.createElement("option")
+    const inactiveStatusOption2 = document.createElement("option")
+
+    const putUserButton = document.createElement('button')
+    putUserButton.innerText = "Update User Data"
+
+    putUserCurrentName.placeholder = "User's Current Name"
+    putUserCurrentEmail.placeholder = "User's Current Email"
+
+    femaleGenderOption2.text = "Female"
+    putUserCurrentGender.add(femaleGenderOption2)
+    maleGenderOption2.text = "Male"
+    putUserCurrentGender.add(maleGenderOption2)
+
+    activeStatusOption2.text = "Active"
+    putUserCurrentStatus.add(activeStatusOption2)
+    inactiveStatusOption2.text = "Inactive"
+    putUserCurrentStatus.add(inactiveStatusOption2)
 
     //DELETE
     const deleteDiv = document.createElement('div')
@@ -85,7 +133,8 @@ function startPageElements() {
     document.body.appendChild(postDiv)
     document.body.appendChild(document.createElement('br'))
     // document.body.appendChild(displayDiv) //why a displayDiv?
-    // document.body.appendChild(document.createElement('br'))
+    document.body.appendChild(putDiv)
+    document.body.appendChild(document.createElement('br'))
     document.body.appendChild(deleteDiv)
 
         //GET
@@ -98,6 +147,16 @@ function startPageElements() {
     postDiv.appendChild(postUserSelectGender)
     postDiv.appendChild(postUserSelectStatus)
     postDiv.appendChild(postUserButton)
+
+        //PUT
+    putDiv.appendChild(putUserInputBox)
+    putDiv.appendChild(putUserCurrentDataButton)
+    putDiv.appendChild(document.createElement('br'))
+    putDiv.appendChild(putUserCurrentName)
+    putDiv.appendChild(putUserCurrentEmail)
+    putDiv.appendChild(putUserCurrentGender)
+    putDiv.appendChild(putUserCurrentStatus)
+    putDiv.appendChild(putUserButton)
 
         //DELETE
     deleteDiv.appendChild(deleteUserInputBox)
@@ -168,9 +227,94 @@ function postUserRequest() {
 
 }
 
-// function patchUserRequest() {
+function putUserDisplayCurrentData() {
+    const children = this.parentElement.children
+    console.log(children)
+    let userID;
+    let minID
+    let maxID
+    for (const element of children) {
+        if (element.type === "number")
+            userID = element.value
+            minID = element.min
+            maxID = element.min
+    }
+    console.log(userID)
 
-// }
+    if (userID === "")
+        return alert("Invalid ID Entered")
+    else if (userID < minID || userID > maxID) {
+        return alert(`User ID must be between ${minID} & ${maxID}`)
+    }
+
+    // const putUserCurrentName =
+
+    //XMLHttpRequest section
+    const endpoint = "https://gorest.co.in/public-api/users/" + userID
+    const xhr = new XMLHttpRequest()
+
+    xhr.open("GET", endpoint)
+
+    xhr.onload = () => {
+        const rawRes = xhr.responseText
+        const parsedData = JSON.parse(rawRes) //for some reason can't use parsedData like I did in the GET method function??
+        console.log(parsedData)
+
+        //fill in all the boxes with the current user data 
+        // console.log("children:")
+        // console.log(children)
+        for (const element of children) {
+            if (element.name === "name")
+                // console.log("this is NAME element")
+                element.defaultValue = parsedData.name
+        }
+
+
+
+
+
+    }
+    xhr.setRequestHeader("Authorization", `Bearer ${myAuthorizationAPIToken}`);
+
+    xhr.send()
+}
+
+
+function putUserDisplayCurrentDataJSONTEST(userID) {
+    
+    const endpoint = "https://gorest.co.in/public-api/users/" + userID
+    const xhr = new XMLHttpRequest()
+
+    xhr.open("GET", endpoint)
+
+    xhr.onload = () => {
+        const rawRes = xhr.responseText
+        const parsedData = JSON.parse(rawRes)
+    }
+    xhr.setRequestHeader("Authorization", `Bearer ${myAuthorizationAPIToken}`);
+
+    xhr.send()
+}
+
+function putUserDisplayCurrentDataJSON(userID) {
+    const endpoint = "https://gorest.co.in/public-api/users/" + userID
+    const xhr = new XMLHttpRequest()
+
+    xhr.open("GET", endpoint)
+
+    xhr.onload = () => {
+        const rawRes = xhr.responseText
+        const parsedData = JSON.parse(rawRes)
+        console.log(parsedData)
+    }
+    xhr.setRequestHeader("Authorization", `Bearer ${myAuthorizationAPIToken}`);
+
+    xhr.send()
+}
+
+function putUserRequest() {
+
+}
 
 function deleteUserRequest() {
     const children = this.parentElement.children
